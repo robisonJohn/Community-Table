1456eaca746fe9072fa8341c4079bb7a84967d9e
johnrobison <robisonj8680@gmail.com>
2021-07-20 12:06:21 -0400

    added item creation function

--- seed/itemData.js
+++ seed/itemData.js
@@ -1,3 +1,17 @@
 import db from '../db/connection.js';
 import Item from '../models/item.js';
-import User from '../models/user.js';
+
+
+const insertItemData = async () => {
+    // reset database
+    await db.dropDatabase()
+
+        const items = [];
+
+        await Item.insertMany(items);
+
+    db.close();
+
+}
+
+insertItemData()
--- seed/userData.js
+++ seed/userData.js
@@ -1,5 +1,4 @@
 import db from '../db/connection.js';
-import Item from '../models/item.js';
 import User from '../models/user.js';
 
 /*
