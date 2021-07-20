d5a391325cb640cedddf8fcf167cc7122cad04ff
andreallata <andrea.alt92@gmail.com>
2021-07-20 14:04:25 -0400

    add routes/index.js file

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import itemsRoutes from './items.js'
+import usersRoutes from './users.js'
+
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is the API root!'))
+
+router.use('/', usersRoutes)
+router.use('/', itemsRoutes)
+
+export default router
