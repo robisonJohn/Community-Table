faed184f91c9b2645db540b84eab5e65e399dcab
johnrobison <robisonj8680@gmail.com>
2021-07-21 12:29:44 -0400

    finalized users.js

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -12,4 +12,31 @@ export const signUp = async (credentials) => {
     }
 }
 
+export const signOut = async () => {
+    try {
+        localStorage.removeItem('token')
+        return true
+    } catch (error) {
+        throw error
+    }
+}
+
+export const changePassword = async (passwords, user) => {
+    try {
+        const resp = await api.post('/')
+        return resp.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+export const verifyUser = async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        const res = await api.get('/verify')
+        return res.data
+    }
+    return false
+}
+
 
