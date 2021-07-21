182635e7f7fc88fb4d8ae77d9673abdf06ce2269
ashleymhuynh <ashley.huynh23@gmail.com>
2021-07-21 09:12:09 -0700

    add layout navbar and footer

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+const authenticatedOptions = (
+  <>
+    <NavLink className="link" to="/add-item">
+      Add to <Table></Table>
+    </NavLink>
+    <NavLink className="link" to="/logout">
+      Logout
+    </NavLink>
+  </>
+);
+const unauthenticatedOptions = (
+  <>
+    <NavLink className="link" to="/sign-up">
+      Sign Up
+    </NavLink>
+    <NavLink className="link" to="/login">
+      Login
+    </NavLink>
+  </>
+);
+
+const alwaysOptions = (
+  <>
+    <NavLink className="link" to="/inventory">
+      Browse Inventory
+    </NavLink>
+  </>
+);
+
+const NavBar = ({ user }) => {
+  return (
+    <nav>
+      <div className="NavBar">
+        <NavLink className="logo" to="/">
+          <img src="https://imgur.com/cyWp9Px" id="logo" />
+        </NavLink>
+        <div className="links">
+          {user && (
+            <div className="link welcome">
+              Welcome, {user.username} from {user.company_name}
+            </div>
+          )}
+          {alwaysOptions}
+          {user ? authenticatedOptions : unauthenticatedOptions}
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
