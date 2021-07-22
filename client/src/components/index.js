21e7f1dc7cb6f501248816d36a7f1c506d6dade7
andreallata <andrea.alt92@gmail.com>
2021-07-22 15:55:00 -0400

    delete ItemCard from index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,4 @@
 import Layout from "./Layout/Layout";
 import Item from "./Item/Item";
 import Sort from "./Sort/Sort";
-import ItemCards from "./ItemCards/ItemCards";
-export { Layout, Item, Sort, ItemCards };
+export { Layout, Item, Sort };
