5cec52747e07ffecf339e32d727de4e4a7e4970b
johnrobison <robisonj8680@gmail.com>
2021-07-26 09:05:13 -0400

    added labels to sign up

--- client/src/screens/ItemDetail/ItemDetail.jsx
+++ client/src/screens/ItemDetail/ItemDetail.jsx
@@ -3,6 +3,7 @@ import { Layout } from '../../components'
 import { getItem, deleteItem } from "../../services/items"
 import { useParams, Link } from "react-router-dom";
 import "./ItemDetail.css"
+// import { Button } from 'react-bootstrap';
 
 const ItemDetail = (props) => {
 const [item, setItem] = useState(null)
@@ -36,9 +37,9 @@ return(
       <div className="category">{item.category}</div>
       <div className="benefits">{item.benefits}</div>
       <div className="quantity"> <h4>Quantity: </h4> {item.quantity}</div>
-      <div className="price">${item.price}</div>
-      <div className="shelfLife">{item.shelfLife}</div>
-      <div className="daysHeld">{item.daysHeld}</div>
+      <div className="price"><h4>Price: </h4>${item.price}</div>
+      <div className="shelfLife"><h4>Shelf Life: </h4>{item.shelfLife}</div>
+      <div className="daysHeld"><h4>Days Held: </h4>{item.daysHeld}</div>
       <div className="box"><p>1 = 1<img src="https://cdn.iconscout.com/icon/free/png-256/box-package-parcel-logistic-delivery-unpack-open-shipping-3-20450.png" alt="box" width="20"/> </p> </div>
   </div>   
   </div>
