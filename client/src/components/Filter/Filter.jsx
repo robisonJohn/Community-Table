5b6f595221751968222419d611899eb90456f82f
johnrobison <robisonj8680@gmail.com>
2021-07-27 08:59:07 -0400

    added React-Bootstrap to Filter component

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,4 +1,5 @@
 import './Filter.css';
+import { Form } from 'react-bootstrap';
 
 function Filter(props) {
     
@@ -7,15 +8,15 @@ function Filter(props) {
     }
 
     return (
-        <form className="filter-container" onSubmit={props.handleSubmit}>
-            <label htmlFor="filter">FILTER BY:</label>
-            <select className="filter" onChange={handleFilter}>
+        <Form className="filter-container" onSubmit={props.handleSubmit}>
+            <Form.Label htmlFor="filter">FILTER BY:</Form.Label>
+            <Form.Select className="filter" onChange={handleFilter}>
                 <option className="option" value="dry-goods">&nbsp; Dry Goods &nbsp;</option>
                 <option className="option" value="produce">&nbsp; Produce &nbsp;</option>
                 <option className="option" value="good-items">&nbsp; Items that are good to sell &nbsp;</option>
                 <option className="option" value="need-to-go">&nbsp; Items that are good to donate  &nbsp;</option>
-            </select>
-        </form>
+            </Form.Select>
+        </Form>
 
     )
 }
