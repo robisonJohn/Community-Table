23a1a179fadb4382473d80fb120e53e8349dbd1e
johnrobison <robisonj8680@gmail.com>
2021-07-27 09:02:39 -0400

    added React-Bootstrap to Sort

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -8,17 +8,17 @@ function Sort(props) {
     }
 
     return (
-        <form className="sort-container" onSubmit={props.handleSubmit}>
-            <label htmlFor="sort">SORT BY:</label>
-            <select className="sort" onChange={handleSort}>
+        <Form className="sort-container" onSubmit={props.handleSubmit}>
+            <Form.Label htmlFor="sort">SORT BY:</Form.Label>
+            <Form.Select className="sort" onChange={handleSort}>
                 <option className="option" value="lowest-quantity">&nbsp; Lowest Quantity to Highest Quantity &nbsp;</option>
                 <option className="option" value="highest-quantity">&nbsp; Highest Quantity to Lowest Quantity &nbsp;</option>
                 <option className="option" value="lowest-price">&nbsp; Lowest Price to Highest Price &nbsp;</option>
                 <option className="option" value="highest-price">&nbsp; Highest Price to Lowest Price &nbsp;</option>
                 <option className="option" value="lowest-shelf-life">&nbsp; Lowest Shelf Life to Highest Shelf Life &nbsp;</option>
                 <option className="option" value="highest-shelf-life">&nbsp; Highest Shelf Life to Lowest Shelf Life &nbsp;</option>
-            </select>
-        </form>
+            </Form.Select>
+        </Form>
     )
 }
 
