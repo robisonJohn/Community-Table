1a5a1b48b7a1e5716b1587aef106eeed7e57c069
johnrobison <robisonj8680@gmail.com>
2021-07-28 09:51:38 -0400

    added the waste constant

--- client/src/utils/filter.js
+++ client/src/utils/filter.js
@@ -1,11 +1,12 @@
 // the filter function should take an array of elements from the api and filter by either dry goods or produce
+const waste = 0.65;
 
 export const filterDryGoods = arr => arr.filter(element => element.category === 'Dry goods');
 export const filterProduce = arr => arr.filter(element => element.category === 'Produce');
 
 export const goodItems = arr => arr.filter(element => 
-    (element.price * (1 - (element.daysHeld / element.shelfLife))) >= element.price * 0.25
+    (element.price * waste * (1 - (element.daysHeld / element.shelfLife))) >= element.price * 0.25
 )
 export const needToGo = arr => arr.filter(element => 
-    element.price * 0.25 > (element.price * (1 - (element.daysHeld / element.shelfLife)))
+    element.price * 0.25 > (element.price * waste * (1 - (element.daysHeld / element.shelfLife)))
 )
